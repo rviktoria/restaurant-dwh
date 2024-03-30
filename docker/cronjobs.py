b64b47d92d6e6c6adaf99f657b4d77d7b93f6281
rviktoria <viktoria.rimko@gmail.com>
2024-03-30 03:28:02 +0200

    Create cron jobs.

---
+++ docker/cronjobs.py
@@ -0,0 +1,25 @@
+import os
+import subprocess
+
+
+# Function to execute Python scripts
+def execute_python_scripts(script_name):
+    script_path = os.path.join('./py_scripts', script_name)
+    print(f"Executing Python script: {script_path}")
+    try:
+        subprocess.run(["python", script_path])
+        print(f"Python script '{script_name}' executed successfully.")
+    except Exception as e:
+        print(f"Error executing Python script {script_name}: {e}")
+
+
+if __name__ == "__main__":
+    
+    # Execute scripts
+    
+    # Load OLAP tables
+    execute_python_scripts('load_stagings.py') #Step 1
+    execute_python_scripts('load_dimensions.py') #Step 2
+    execute_python_scripts('load_facts.py') #Step 3
+
+    
